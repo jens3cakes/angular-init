7e2182fb3e790bfb9cdb03edc4d54ff7ed7276a5
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-11-27 18:54:36 -1000

    git init

---
+++ src/app/hk/hk.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-hk',
+  templateUrl: './hk.component.html',
+  styleUrls: ['./hk.component.scss']
+})
+
+export class HkComponent{
+  
+}
